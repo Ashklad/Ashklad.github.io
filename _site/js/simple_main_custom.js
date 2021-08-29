7e18624db4ed502f1fc4d31f7ca185dd400ac553
Ashklad <89686032+Ashklad@users.noreply.github.com>
2021-08-29 14:26:29 +0900

    info fin / canvas set

--- _site/js/simple_main_custom.js
+++ _site/js/simple_main_custom.js
@@ -1,17 +1,66 @@
 
 $(document).ready(function() {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    let WIDTH = canvas.width
+    let HEIGHT = canvas.height
+
     function draw() {
+        // 기존 이미지 초기화
+        var canvas = document.getElementById("canvas");
+        var ctx = canvas.getContext("2d");
+        ctx.beginPath();
+        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
 
-       var profile_img = $('#profile_img');
+        // 이미지 생성
+        var canvas = document.getElementById("canvas");
+        var ctx = canvas.getContext("2d");
+        ctx.font = '40px NexonLv2GothicBold';
+        ctx.fillText('마비노기 트친소 시트', 50, 80);
 
-       // 이미지 생성
-       var canvas = document.getElementById("canvas");
-       if (!canvas.getContext) {
-           return;
-       }
-       var ctx = canvas.getContext("2d");
-       ctx.font = '48px serif';
-       ctx.fillText('Hello world', 10, 50);
+        // https://lts0606.tistory.com/485 참고하기
 
     }
-}
+
+    function download(canvas, prefix) {
+        var link = document.createElement('a');
+        link.download = prefix + Date.now() + '.png';
+        link.href = document.getElementById('canvas').toDataURL()
+        link.click();
+    }
+
+    $('#draw').click(function() {
+        draw();
+    });
+
+    $('#download').click(function() {
+        download(document.getElementById('canvas'), 'mabi_');
+    });
+
+    $("input").change(function() {
+        setTimeout(function() {
+            draw();
+        }, 100);
+    });
+    $('input[type=checkbox], select').change(draw);
+	$('input[type=select], change').change(draw);
+
+	const profile_img = document.getElementById("profile_img");
+	profile_img.addEventListener('change', function (event) {
+        let reader = new FileReader();
+        reader.onload = function (e){
+            img = new Image();
+            img.src = e.target.result
+            img.onload = function(){
+                ctx.save()
+                ctx.drawImage(img, 160, 160, 160, 160);
+                ctx.restore()
+        }
+        };
+        reader.readAsDataURL(event.target.files[0])
+    });
+
+    setTimeout(function() {
+        draw();
+    }, 1000);
+});
